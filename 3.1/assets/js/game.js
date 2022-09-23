a3eaa74028d064c3d4c1144aab3263b274605afa
amiller0806 <ariel.miller555@gmail.com>
2022-09-23 13:25:39 -0400

    initial game setup.

--- 3.1/assets/js/game.js
+++ 3.1/assets/js/game.js
@@ -15,10 +15,10 @@ var enemyAttack = 12;
 var fight = function() {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
-
   // ask player if they'd like to fight or run
-  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
+  
+var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
   // if player choses to fight, fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
